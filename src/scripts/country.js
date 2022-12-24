9008cf8ab987d2c44957304eb397e167d1a6a559
João Capucho <jcapucho7@gmail.com>
2022-12-24 20:32:02 +0000

    Add map to country page

--- src/scripts/country.js
+++ src/scripts/country.js
@@ -1,5 +1,29 @@
 const countryId = new URLSearchParams(window.location.search).get("id");
 
+async function loadMap(country) {
+    const params = new URLSearchParams({ country, format: "json", polygon_geojson: 1 });
+    const response = await fetch("https://nominatim.openstreetmap.org/search?" + params);
+    const data = await response.json();
+
+    const result = data[0];
+
+    const lat = result.lat;
+    const lon = result.lon;
+
+    const map = L.map('map').setView([lat, lon], 5);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+    L.geoJson(result.geojson).addTo(map);
+
+    document.getElementById("mapModal").addEventListener('shown.bs.modal', function() {
+        setTimeout(function() {
+            map.invalidateSize();
+        }, 10);
+    });
+}
+
 function CountryViewModel() {
     const self = this;
 
@@ -13,6 +37,8 @@ function CountryViewModel() {
         const data = await response.json();
         console.log(data);
         self.country(data);
+
+        loadMap(data.Name);
     }
 
     self.country.subscribe(function(newValue) {
