13db59a110107ba221efd0ae61987854f12363f6
João Capucho <jcapucho7@gmail.com>
2023-01-01 15:31:52 +0000

    Allow filtering games by season

--- flake.nix
+++ flake.nix
@@ -2,14 +2,16 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = {
-    nixpkgs,
-    flake-utils,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
+  outputs =
+    { nixpkgs
+    , flake-utils
+    , ...
+    }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
       pkgs = nixpkgs.legacyPackages.${system};
-    in {
+    in
+    {
       devShells.default = pkgs.mkShell {
         buildInputs = with pkgs; [
           nodejs
--- src/scripts/games.js
+++ src/scripts/games.js
@@ -7,8 +7,22 @@ function CompetitionsViewModel() {
 
   self.games = ko.observableArray([]);
 
+  self.season = ko.observable("0");
+
   self.loader = createListLoader(self.games, "Games", favoritesSection);
   self.toggleFavorite = favoriteToggle(favoritesSection);
+
+  self.season.subscribe(function(value) {
+    const season = event.target.value;
+    viewModel.loader.reset();
+
+    if (season !== "0")
+      viewModel.loader.extraParams = { season };
+    else
+      viewModel.loader.extraParams = null;
+
+    viewModel.loader.loadMore();
+  });
 }
 
 const viewModel = new CompetitionsViewModel();
